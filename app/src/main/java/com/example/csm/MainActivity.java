0e8ec457402b0f2f58aa9fa5675b8f3594f0c0bc
DanilRumyantsev <danilrumancev48@gmail.com>
2023-11-18 01:35:31 +0300

    global commit

---
+++ app/src/main/java/com/example/csm/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.csm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private EditText user_field;
+    private Button main_btn;
+    private TextView result_info;
+    private TextToSpeech TTS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user_field = findViewById(R.id.user_field);
+        main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
+
+        // Инициализация TextToSpeech
+        TTS = new TextToSpeech(this, this);
+
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(user_field.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
+                else {
+                    // Получение текста из поля и преобразование в речь
+                    String text = user_field.getText().toString();
+                    speakText(text);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        if (TTS != null) {
+            TTS.stop();
+            TTS.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = TTS.setLanguage(Locale.getDefault());
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Language not supported", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Initialization failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void speakText(String text) {
+        TTS.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+}
