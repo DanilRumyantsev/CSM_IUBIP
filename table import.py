4c4e91e419250d2da48475287400afa5e9da4393
Anatoly <126594092+ShaobiShain@users.noreply.github.com>
2023-11-17 21:19:49 +0300

    Add files via upload

--- table import.py
+++ table import.py
@@ -1,22 +1,24 @@
 import csv
 
-def read_values_from_csv(filename):
-    values = {}
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            values[row[0]] = row[1]
-    return values
+filename = 'test.csv'  # Укажите имя вашего CSV-файла
 
-filename = 'your_file.csv'  # Укажите имя вашего CSV-файла
+with open(filename, newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    data = list(reader)
+  value = data[0][0]
+   value2 = data[0][1]
+    value3 = data[0][2]
+     value3 = data[0][3]
+      value3 = data[0][4]
+       value3 = data[0][5]
+        value3 = data[0][6]
+         value3 = data[0][7]
+          value3 = data[0][8]
+           value3 = data[0][9]
+            value3 = data[0][10]
+             value3 = data[0][11]
+              value3 = data[0][12]
+               value3 = data[0][13]
+                value3 = data[0][14]
 
-# Чтение значений из CSV-файла
-values = read_values_from_csv(filename)
-
-# Использование значений
-for key, value in values.items():
-    exec(f"{key} = '{value}'")  # Создание переменной из имени в CSV и присваивание значения
-
-# Вывод значений переменных
-print(a)  # Выведет 'hello'
-print(b)  # Выведет 'bay'
+    print(value2)
